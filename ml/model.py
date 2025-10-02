e9963db9f4462fd1987cf550282cfaa706ee498c
eenpearce <ianhooper0@gmail.com>
2025-10-02 09:10:28 -0600

    My mistake. I was editing the files locally with VSCode, and WSL did not like that. I have now implemented the functions properly.

--- ml/model.py
+++ ml/model.py
@@ -1,12 +1,11 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
-
+import joblib
+from sklearn.ensemble import RandomForestClassifier
 # TODO: add necessary import
 
 # Optional: implement hyperparameter tuning.
-
-
 def train_model(X_train, y_train):
     """
     Trains a machine learning model and returns it.
@@ -22,8 +21,9 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    # TODO: implement the function
-    pass
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -49,7 +49,7 @@ def compute_model_metrics(y, preds):
 
 
 def inference(model, X):
-    """Run model inferences and return the predictions.
+    """ Run model inferences and return the predictions.
 
     Inputs
     ------
@@ -62,12 +62,11 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
-
+    preds = model.predict(X)
+    return preds
 
 def save_model(model, path):
-    """Serializes model to a file.
+    """ Serializes model to a file.
 
     Inputs
     ------
@@ -76,20 +75,17 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
-
+    joblib.dump(model, path)
 
 def load_model(path):
-    """Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+    """ Loads pickle file from `path` and returns it."""
+    return joblib.load(path)
 
 
 def performance_on_categorical_slice(
     data, column_name, slice_value, categorical_features, label, encoder, lb, model
 ):
-    """Computes the model metrics on a slice of the data specified by a column name and
+    """ Computes the model metrics on a slice of the data specified by a column name and
 
     Processes the data using one hot encoding for the categorical features and a
     label binarizer for the labels. This can be used in either training or
@@ -122,14 +118,18 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    # TODO: implement the function
+    data_slice = data[data[column_name] == slice_value]
+    if data_slice.shape[0] == 0:
+        return None
+
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value
-        # use training = False
+        data_slice,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb,
     )
-    preds = (
-        None  # your code here to get prediction on X_slice using the inference function
-    )
+    preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
